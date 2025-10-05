885d927c61ff96367e44dfaa768accfeaf9fda12
MorozkoArt <artemmorozov155@gmail.com>
2025-10-05 19:09:38 +0300

    created a file that will be responsible for connecting to PostgreSQL

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -4,8 +4,31 @@ import (
 	"context"
 	"fmt"
 	"log"
+	
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/morozkoart/go-crud-api/internal/config"
+	"github.com/MorozkoArt/go-crud-api/internal/config"
 )
 
+func NewPostgreDB(ctx context.Context, cfg *config.Config) (*pgxpool.Pool, error) {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		cfg.Database.User,
+		cfg.Database.Password,
+		cfg.Database.Host,
+		cfg.Database.Port,
+		cfg.Database.Name,
+		cfg.Database.SSLMode,
+	)
 
+	pool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		return nil, err
+	}
+
+	log.Println("Connection to PostgreSQL established")
+	return pool, nil
+}
