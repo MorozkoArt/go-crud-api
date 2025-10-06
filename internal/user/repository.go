92f2222cf4630bf5864c5603f2b3347f16083132
MorozkoArt <artemmorozov155@gmail.com>
2025-10-06 19:41:59 +0300

    added a meathod to the repository that returns all users

--- internal/user/repository.go
+++ internal/user/repository.go
@@ -19,3 +19,26 @@ func (r *Repository) Create(ctx context.Context, u *User) error {
 		u.Name, u.Email, u.Password)
 	return err
 }
+
+func (r *Repository) GetAll(ctx context.Context) ([]User, error) {
+	rows, err := r.db.Query(ctx, "Select id, name, email, FROM users ORDER BY id")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var users []User
+
+	for rows.Next() {
+		var u User
+		if err := rows.Scan(&u.Id, &u.Name, &u.Email); err != nil {
+			return nil, err
+		}
+		users = append(users, u)
+	}
+
+	return users, nil
+
+}
